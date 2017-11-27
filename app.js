e1f63938a7e8eb24c2d809a13917c2dd47b12754
maksymko658 <Jmaksy@ukr.net>
2017-11-27 06:51:34 +0200

    . forgot

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const Nexmo = require('nexmo');
-const socketio = require('socketio');
+const socketio = require('socket.io');
 
 //Init app
 const app = express();
@@ -14,3 +14,9 @@ app.use(express.static(__dirname + '/public'));
 //Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Define port
+const port = 3000;
+
+//Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
