7a3aa207e32c5321ddfcc313ddc43705262201a9
maksymko658 <Jmaksy@ukr.net>
2017-11-29 02:39:05 +0200

    use fetchAPI

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,19 @@ const numberInput = document.getElemntById('number'),
  textInput = document.getElemntById('msg'),
  button = document.getElemntById('button'),
  response = document.querySelector('.response');
- 
+
+
+ button.addEventListener('click', send, false);
+
+ function send(){
+ 	const number = numberInput.value.replace(/\D/g, '')
+ 	const text = textInput.value;
+
+ 	fetch('/', {
+ 		method: 'post',
+ 		headers: {
+ 			'Content-type': 'application/json'
+ 		},
+ 		body: {JSON.stringify({number: number, text: text})}
+ 	})
+ }
