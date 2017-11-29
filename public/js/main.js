e14c62c2c737fb9e779b5150940d3734afcbaac7
maksymko658 <Jmaksy@ukr.net>
2017-11-29 02:23:54 +0200

    add all our UI variables

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,5 @@
 const numberInput = document.getElemntById('number'),
  textInput = document.getElemntById('msg'),
+ button = document.getElemntById('button'),
+ response = document.querySelector('.response');
+ 
